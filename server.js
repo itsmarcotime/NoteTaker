a88eea3fe027782a3d92c9190e64650c3f2c5d7c
Marco Mata <patrickstar1281@yahoo.com>
2022-07-17 11:48:06 -0500

    linked html to app using get express

---
+++ server.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const {db} = require('./db/db.json');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/api/db', (req, res) => {
+    res.json(db);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
